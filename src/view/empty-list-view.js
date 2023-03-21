1a77617be78768ed62bf882908a63aefdd6bfcc0
Artemonkey <101344196+Artemonkey@users.noreply.github.com>
2023-03-21 18:43:26 +0500

    Исправления

--- src/view/empty-list-view.js
+++ src/view/empty-list-view.js
@@ -1,22 +1,21 @@
 import {createElement} from '../render.js';
 
 const createListEmptyTemplate = () =>
-    `<main class="page-body__page-main  page-main">
-      <div class="page-body__container">
-        <section class="trip-events">
-           <h2 class="visually-hidden">Trip events</h2>
-           <p class="trip-events__msg">Click New Event to create your first point</p>
-           <!--
-             Значение отображаемого текста зависит от выбранного фильтра:
-            * Everthing – 'Click New Event to create your first point'
-            * Past — 'There are no past events now';
-            * Present — 'There are no present events now';
-            * Future — 'There are no future events now'.
-           -->
-        </section>
-      </div>
-    </main>`;
-    
+  `<main class="page-body__page-main  page-main">
+    <div class="page-body__container">
+      <section class="trip-events">
+         <h2 class="visually-hidden">Trip events</h2>
+         <p class="trip-events__msg">Click New Event to create your first point</p>
+         <!--
+           Значение отображаемого текста зависит от выбранного фильтра:
+          * Everthing – 'Click New Event to create your first point'
+          * Past — 'There are no past events now';
+          * Present — 'There are no present events now';
+          * Future — 'There are no future events now'.
+         -->
+      </section>
+    </div>
+  </main>`;
 export default class ListEmptyView {
   getTemplate() {
     return createListEmptyTemplate();
