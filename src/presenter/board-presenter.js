57c0002ff2e5238df383e0f388680abbd9f298c3
Artemonkey <101344196+Artemonkey@users.noreply.github.com>
2023-03-28 13:53:48 +0500

    Исправления после комментариев наставника 1

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,21 +1,20 @@
 import BoardView from '../view/board-view.js';
 import FiltersView from '../view/filters-view';
 import EventItemView from '../view/event-item-view';
-import {RenderPosition, render,} from '../render.js';
+import {RenderPosition, render} from '../render.js';
 import EventEditList from '../view/event-edit-item-view.js';
 import SortView from '../view/sort-view.js';
 import EventsListView from '../view/events-list-view.js';
 
 export default class Presenter {
-  boardComponent = new BoardView();
-  filterComponent = new FiltersView();
-  sortComponent = new SortView();
-  eventEditListComponent = new EventEditList();
-  eventsList = new EventsListView();
-
   constructor({boardContainer, bodyContainer}) {
     this.boardContainer = boardContainer;
     this.bodyContainer = bodyContainer;
+    this.boardComponent = new BoardView();
+    this.filterComponent = new FiltersView();
+    this.sortComponent = new SortView();
+    this.eventEditListComponent = new EventEditList();
+    this.eventsList = new EventsListView();
   }
 
   init() {
@@ -24,7 +23,6 @@ export default class Presenter {
     render(this.sortComponent, this.bodyContainer);
     render(this.eventsList, this.bodyContainer);
     render(this.eventEditListComponent, this.eventsList.getElement());
-    // render(new EventEditView(), this.taskListComponent.getElement());
 
     for (let i = 0; i < 3; i++) {
       render(new EventItemView(), this.eventsList.getElement());
