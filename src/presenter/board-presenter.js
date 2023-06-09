25a6aa3051cd96d4e2e6574e06cca96e4058c4f9
Artiom Iambushev <yambushev45@gmail.com>
2023-06-09 21:17:13 +0500

    Шаблонизируй это

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -5,6 +5,7 @@ import {RenderPosition, render} from '../render.js';
 import EventEditList from '../view/event-edit-item-view.js';
 import SortView from '../view/sort-view.js';
 import EventsListView from '../view/events-list-view.js';
+import EventsModel from '../model/events-model.js';
 
 export default class Presenter {
   constructor({boardContainer, bodyContainer}) {
@@ -15,17 +16,22 @@ export default class Presenter {
     this.sortComponent = new SortView();
     this.eventEditListComponent = new EventEditList();
     this.eventsList = new EventsListView();
+    this.eventModel = new EventsModel();
   }
 
   init() {
+    const events = this.eventModel.getEvents();
+    const destinations = this.eventModel.getDestinations();
+    const offersByType = this.this.eventModel.getOffersByType();
+
     render(this.boardComponent, this.boardContainer, RenderPosition.AFTERBEGIN);
     render(this.filterComponent, this.boardContainer,);
     render(this.sortComponent, this.bodyContainer);
     render(this.eventsList, this.bodyContainer);
-    render(this.eventEditListComponent, this.eventsList.getElement());
+    render(this.eventEditListComponent(events[0], destinations, offersByType), this.eventsList.getElement());
 
-    for (let i = 0; i < 3; i++) {
-      render(new EventItemView(), this.eventsList.getElement());
+    for (const event of events) {
+      render(new EventItemView(event, destinations, offersByType), this.eventsList.getElement());
     }
 
   }
--- src/view/event-item-view.js
+++ src/view/event-item-view.js
@@ -1,47 +1,65 @@
 import {createElement} from '../render.js';
 
-const createEventItemTemplate = () =>
-  `<li class="trip-events__item">
-    <div class="event">
-      <time class="event__date" datetime="2019-03-18">MAR 18</time>
-      <div class="event__type">
-        <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
-      </div>
-      <h3 class="event__title">Taxi Amsterdam</h3>
-      <div class="event__schedule">
-        <p class="event__time">
-          <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
-          &mdash;
-          <time class="event__end-time" datetime="2019-03-18T11:00">11:00</time>
+const createEventItemTemplate = (event, destinations, offersByType) => {
+  const eventDestination = destinations.find((dest) => dest.id === event.destinations);
+  const eventTypeOffers = offersByType.find((off) => off.type === event.type).offers;
+  const eventOffers = eventTypeOffers.filter((offer) => event.offer.includes(offer.id));
+
+  return (
+    `<li class="trip-events__item">
+      <div class="event">
+        <time class="event__date" datetime="${'2019-03-18'}">${'MAR 18'}</time>
+        <div class="event__type">
+          <img class="event__type-icon" width="42" height="42" src="img/icons/${event.type}.png" alt="Event type icon">
+        </div>
+        <h3 class="event__title">${event.type} ${eventDestination.name}</h3>
+        <div class="event__schedule">
+          <p class="event__time">
+            <time class="event__start-time" datetime="${'2019-03-18T10:30'}">${'10:30'}</time>
+            &mdash;
+            <time class="event__end-time" datetime="${'2019-03-18T11:00'}">${'11:00'}</time>
+          </p>
+          <p class="event__duration">30M</p>
+        </div>
+        <p class="event__price">
+          &euro;&nbsp;<span class="event__price-value">${event.basePrice}</span>
         </p>
-        <p class="event__duration">30M</p>
+        <h4 class="visually-hidden">Offers:</h4>
+        <ul class="event__selected-offers">
+
+   ${eventOffers.map((offer) => (
+      `<li class="event__offer">
+        <span class="event__offer-title">${offer.title}</span>
+        &plus;&euro;&nbsp;
+        <span class="event__offer-price">${offer.price}</span>
+      </li>`
+    )).join('')}
+          
+        </ul>
+        <button class="event__favorite-btn ${event.isFavorite ? 'event__favorite-btn--active' : ''}  type="button">
+          <span class="visually-hidden">Add to favorite</span>
+          <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
+            <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"/>
+          </svg>
+        </button>
+        <button class="event__rollup-btn" type="button">
+          <span class="visually-hidden">Open event</span>
+        </button>
       </div>
-      <p class="event__price">
-        &euro;&nbsp;<span class="event__price-value">20</span>
-      </p>
-      <h4 class="visually-hidden">Offers:</h4>
-      <ul class="event__selected-offers">
-        <li class="event__offer">
-          <span class="event__offer-title">Order Uber</span>
-          &plus;&euro;&nbsp;
-          <span class="event__offer-price">20</span>
-        </li>
-      </ul>
-      <button class="event__favorite-btn event__favorite-btn--active" type="button">
-        <span class="visually-hidden">Add to favorite</span>
-        <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
-          <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"/>
-        </svg>
-      </button>
-      <button class="event__rollup-btn" type="button">
-        <span class="visually-hidden">Open event</span>
-      </button>
-    </div>
-  </li>`;
+    </li>`
+  );
+};
 
 export default class EventItemView {
+  constructor(event, destinations, offersByType) {
+    this.event = event;
+    this.destinations = destinations;
+    this.offersByType = offersByType;
+  }
+
+
   getTemplate() {
-    return createEventItemTemplate();
+    return createEventItemTemplate(this.event, this.destinations, this.offersByType);
   }
 
   getElement() {
