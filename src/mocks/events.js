79770a89d5ba4dc08a32d98489099b1fcce0eafb
Artiom Iambushev <yambushev45@gmail.com>
2023-06-09 21:18:13 +0500

    Шаблонизируй это

---
+++ src/mocks/events.js
@@ -0,0 +1,12 @@
+export const events = [
+  {
+    'basePrice': 1100,
+    'dateFrom': '2019-07-10T22:55:56.845Z',
+    'dateTo': '2019-07-11T11:22:13.375Z',
+    'destination': 0,
+    'id': '0',
+    'isFavorite': false,
+    'offers': [],
+    'type': 'bus'
+  }
+];
